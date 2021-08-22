7ab7d7dde010e26081e58efaa49bea14f2782956
alamin <alamin@gmail.com>
2021-08-22 16:37:31 +0600

    fetch data succesfully

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
-const options = {
-  method: "GET",
-  url: "https://community-open-weather-map.p.rapidapi.com/weather",
-  params: {
-    q: "Dhaka",
-    cnt: "0",
-    mode: "null",
-    lon: "0",
-    type: "link, accurate",
-    lat: "0",
-    units: "imperial, metric",
-  },
-  headers: {
-    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-    "x-rapidapi-key": "bb5faa6a04msh458e2202ef7e301p1a95ddjsn4e8e618febd5",
-  },
-};
-
 function App() {
+  const [weatherinfo, setWeatherInfo] = useState("");
+  const inputRef = useRef("");
+
   useEffect(() => {
-    axios
-      .request(options)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    fetchWeatherInfo();
   }, []);
+
+  const fetchWeatherInfo = async (e) => {
+    e?.preventDefault();
+
+    const options = {
+      method: "GET",
+      url: "https://community-open-weather-map.p.rapidapi.com/weather",
+      params: {
+        q: inputRef.current.value || "Dhaka",
+        units: "metric",
+      },
+      headers: {
+        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+        "x-rapidapi-key": "bb5faa6a04msh458e2202ef7e301p1a95ddjsn4e8e618febd5",
+      },
+    };
+
+    const response = await axios.request(options);
+    setWeatherInfo(response.data);
+  };
 
   return (
     <div>
       <h1>Hello world</h1>
+      <form>
+        <input ref={inputRef} type="text" placeholder="Type the city" />
+        <button onClick={fetchWeatherInfo} type="submit">
+          Click me
+        </button>
+      </form>
+      <h2>{weatherinfo.name}</h2>
     </div>
   );
 }
